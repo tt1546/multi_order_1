7e20fcd80137faac1aa418708e6244c57d546ef5
tt1546 <lyuxintong1012@gmail.com>
2025-05-02 03:51:49 +0800

    Update 5_Model_Predictor.py

--- pages/5_Model_Predictor.py
+++ pages/5_Model_Predictor.py
@@ -70,7 +70,7 @@ input_features = pd.DataFrame([{
 @st.cache_resource
 def load_model():
     model = xgb.Booster()
-    model.load_model("path/to/model/xgb_model.json")
+    model.load_model("models/xgb_model.json")
     return model
 
 model = load_model()
